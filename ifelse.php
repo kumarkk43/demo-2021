cee4e56814a8578f2cb270739f014ee618a65ff1
git <shabeerk@notionpress.com>
2021-06-11 03:04:08 +0000

    new

---
+++ ifelse.php
@@ -0,0 +1,18 @@
+<?php
+// speed in kmph
+$speed = 110;
+
+if($speed < 60)
+{
+    echo "Safe driving speed";
+}
+elseif($speed > 60 && $speed < 100)
+{
+    echo "You are burning extra fuel";
+}
+else 
+{
+    // when speed is greater than 100
+    echo "Its dangerous";
+}
+?>
